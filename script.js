473122ce5ab614b5ba6e0ad3f954cfc0dfff5c29
23cill <botaktaa7@gmail.com>
2026-07-12 14:31:10 +0700

    Tambah grafik equity

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let trades = JSON.parse(localStorage.getItem("trades")) || [];
 
+let balance = Number(localStorage.getItem("balance")) || 1000;
+
 const form = document.getElementById("tradeForm");
 
 form.addEventListener("submit", function(e){
@@ -97,3 +99,46 @@ a.click();
 });
 
 renderTable();
+updateBalance();
+
+function updateBalance() {
+
+let totalProfit = trades.reduce(
+(sum,t)=>sum+t.profit,0
+);
+
+document.getElementById("currentBalance").innerText =
+"$" + (balance + totalProfit).toFixed(2);
+
+drawChart();
+}
+
+function drawChart(){
+
+const ctx =
+document.getElementById("equityChart");
+
+let equity = balance;
+let data = [];
+
+trades.forEach(t=>{
+equity += t.profit;
+data.push(equity);
+});
+
+if(window.myChart){
+window.myChart.destroy();
+}
+
+window.myChart = new Chart(ctx,{
+type:'line',
+data:{
+labels:data.map((_,i)=>`Trade ${i+1}`),
+datasets:[{
+label:'Equity Curve',
+data:data,
+tension:0.3
+}]
+}
+});
+  }
